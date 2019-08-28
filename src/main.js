5ca789017c7820eb2c13f09a994f13c54c4e39b5
jingboya <jingboya666@163.com>
2019-08-28 16:58:55 +0800

    commit message

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+// 优先查找文件，如果文件查找不到，则找目录
+// 找到目录，优先加载目录中的index.js
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
